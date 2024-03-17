5d07abff2d6b2143246daf89afa785dbeed61864
anthrofax <afridhoikhsan@gmail.com>
2024-03-17 19:54:30 +0700

    Exercise completed - Adding new input & Cancel add new expense button

--- lib/widgets/new_expenses.dart
+++ lib/widgets/new_expenses.dart
@@ -17,10 +17,12 @@ class _NewExpansesState extends State<NewExpenses> {
   // }
 
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -36,11 +38,27 @@ class _NewExpansesState extends State<NewExpenses> {
             maxLength: 50,
             decoration: const InputDecoration(label: Text("Expense Title")),
           ),
-          ElevatedButton(
-              onPressed: () {
-                print(_titleController.text);
-              },
-              child: const Text("Save Expense"))
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: _amountController,
+            decoration:
+                const InputDecoration(label: Text("Amount"), prefixText: "\$ "),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(onPressed: () {}, child: const Text("Cancel")),
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: const Text("Save Expense"))
+            ],
+          )
         ],
       ),
     );
