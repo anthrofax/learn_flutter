204c3e099618aa87cae41e90de5bf620ca5ed87c
anthrofax <afridhoikhsan@gmail.com>
2024-03-17 11:56:23 +0700

    Introducing an Enum data type

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,12 +1,14 @@
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
+enum Category {food, work, leisure, travel}
 
 class Expense {
-  Expense({required this.title, required this.amout, required this.date}) : id = uuid.v4();
+  Expense({required this.title, required this.amout, required this.date, required this.category}) : id = uuid.v4();
 
   final String id;
   final String title;
   final int amout;
   final DateTime date;
+  final Category category;
 }
