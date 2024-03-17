845dcedd7bcecc5659bf7d1a6b5264a76dbbc3cd
anthrofax <afridhoikhsan@gmail.com>
2024-03-17 16:31:41 +0700

    Adding app bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:learn_flutter/widgets/expenses.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Expenses(),
     ),
   );
