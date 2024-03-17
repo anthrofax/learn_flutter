2593fc04fa0f281a55ab895fe1be9e63da6d0421
anthrofax <afridhoikhsan@gmail.com>
2024-03-17 15:14:46 +0700

    Linking category with related icon by using Map & Creating getter method to get Formatted Date string

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,14 +1,35 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final DateFormat formatter = DateFormat.yMd();
 
 const uuid = Uuid();
-enum Category {food, work, leisure, travel}
+
+enum Category { food, work, leisure, travel }
+
+final Map categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.work: Icons.work,
+  Category.leisure: Icons.movie,
+  Category.travel: Icons.flight_takeoff
+};
 
 class Expense {
-  Expense({required this.title, required this.amout, required this.date, required this.category}) : id = uuid.v4();
+  Expense(
+      {required this.title,
+      required this.amout,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
 
   final String id;
   final String title;
   final int amout;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
--- lib/widgets/expense_list/expense_item.dart
+++ lib/widgets/expense_list/expense_item.dart
@@ -9,25 +9,31 @@ class ExpenseItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [
-          Text(expense.title),
-          Row(
-            children: [
-              Text("\$${expense.amout.toStringAsFixed(2)}"),
-              const Spacer(),
-               Row(
-                children: [
-                  const Icon(Icons.alarm),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Text(expense.date.toString()),
-                ],
-              )
-            ],
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16
+        ),
+        child: Column(
+          children: [
+            Text(expense.title),
+            Row(
+              children: [
+                Text("\$${expense.amout.toStringAsFixed(2)}"),
+                const Spacer(),
+                 Row(
+                  children: [
+                     Icon(categoryIcons[expense.category]),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(expense.formattedDate),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
