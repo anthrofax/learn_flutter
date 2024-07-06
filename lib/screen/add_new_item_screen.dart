0c7ca89729750f685138ba3136b261c7699dd0b2
anthrofax <afridhoikhsan@gmail.com>
2024-07-06 05:00:16 +0000

    Adding the http Package

--- lib/screen/add_new_item_screen.dart
+++ lib/screen/add_new_item_screen.dart
@@ -1,3 +1,5 @@
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/data/categories.dart';
 import 'package:learn_flutter/models/category.dart';
