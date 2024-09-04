142f3f4d4b0ad5aaa44c5336016f5c46867d82f1
anthrofax <afridhoikhsan@gmail.com>
2024-09-04 18:58:03 +0000

    246. Using the Device Camera For Taking Pictures

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -10,21 +13,51 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _selectedImage;
+
+  void _takeImage() async {
+    final imagePicker = ImagePicker();
+
+    final pickedImage = await imagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    if (pickedImage == null) return;
+
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      onPressed: _takeImage,
+      label: const Text("Add Image"),
+      icon: const Icon(Icons.camera),
+    );
+
+    if (_selectedImage != null) {
+      content = GestureDetector(
+        onTap: _takeImage,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
+
     return (Container(
-      decoration: BoxDecoration(
-          border: Border.all(
-              width: 2,
-              color: Theme.of(context).colorScheme.primary.withOpacity(0.2))),
-      height: 250,
-      width: double.infinity,
-      alignment: Alignment.center,
-      child: TextButton.icon(
-        onPressed: () {},
-        label: const Text("Add Image"),
-        icon: const Icon(Icons.camera),
-      ),
-    ));
+        decoration: BoxDecoration(
+            border: Border.all(
+                width: 2,
+                color: Theme.of(context).colorScheme.primary.withOpacity(0.2))),
+        height: 250,
+        width: double.infinity,
+        alignment: Alignment.center,
+        child: content));
   }
 }
