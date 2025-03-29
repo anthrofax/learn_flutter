98c3690534d6cd9f6ab6b27a539dd2066cbba3eb
anthrofax <afridhoikhsan@gmail.com>
2025-03-30 00:43:09 +0700

    282. Adding User Logout

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
+
+  Future<void> _signOut(BuildContext context) async {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Logout'),
+        content: const Text('Apakah Anda yakin ingin logout?'),
+        actions: [
+          TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pop(context);
+            },
+            child: const Text('Ya'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Tidak'),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('FlutterChat'),
+        actions: [
+          IconButton(
+            onPressed: () => _signOut(context),
+            icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Logged in'),
