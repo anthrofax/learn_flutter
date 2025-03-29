f9ab81a31d89777f7166f2264b9d78140835e772
anthrofax <afridhoikhsan@gmail.com>
2025-03-30 00:15:59 +0700

    280. Showing Different Screens Based On The Authentication State

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:favorite_places/screens/auth.dart';
+import 'package:favorite_places/screens/auth_screen.dart';
+import 'package:favorite_places/screens/chat_screen.dart';
+import 'firebase_options.dart';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +26,18 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
-      home: const AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // Jika ada data (pengguna telah login), tampilkan ChatScreen
+            return const ChatScreen();
+          } else {
+            // Jika tidak ada data (pengguna belum login), tampilkan AuthScreen
+            return const AuthScreen();
+          }
+        },
+      ),
     );
   }
 }
